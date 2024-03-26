ae6e98719a67420a6d9cd9917c29a02d3330210a
Skypeace8585 <i.takahashi.sys22@morijyobi.ac.jp>
2024-03-26 10:46:56 +0900

    indexの作成

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>index</title>
+    <title>メニュー一覧</title>
 </head>
 <body>
-<h1>index</h1>
+<h1>メニュー</h1>
 </body>
 </html>
