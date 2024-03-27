d65e5f724c1987c1e1be2f750cde06a147244ea7
kamata keigo <k.kamata.sys23@morijyobi.ac.jp>
2024-03-27 09:55:49 +0900

    ログイン画面の元の作成

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -13,5 +13,52 @@
         <a href="/register">登録</a>)</p>
 @endif
     <a href="/home">ログアウト</a>
+
+
+@csrf
+    <header>
+        <a href="#">
+        <a href="#">
+        <a href="#">
+        <a href="#">
+        <a href="#">
+        <a href="#">
+        <a href="#">
+        <a href="#">
+        <a href="#">
+        <a href="#">
+        <a href="#">
+        <a href="#">
+    </header>
+    <div>
+        <form action="#">
+            Mail:<input type="email" id="mail" name="mail" placeholder="メールアドレスを入力してください"><br>
+            PW:<input type="password" id="password" name="password" placeholder="パスワードを入力してください"><br>
+            <input type="submit" value="ログイン">
+        </form>
+    </div>
+        <tr>
+            <th>名前</th>
+            <th>値段(税込)</th>
+            <th>画像</th>
+        </tr>
+    
+        @foreach($products as $product)
+            <tr>
+                <th>$product -> name</th>
+                <th>$product -> price_tax</th>
+                <th>$product -> image</th>
+            </tr>
+        @endforeach
+
+
+        <footer>
+        <div>
+            <a href="#" >
+            <a href="#" >
+            <a href="#" >
+            <a href="#" >
+        </div>
+        </footer> 
 </body>
 </html>
